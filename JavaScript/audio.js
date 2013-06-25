14d6673b50c3e75f2e0bf0dd4dbee2e61264d06d
Michael Dugan <mjdugan14@gmail.com>
2013-06-25 12:30:48 -0400

    Initial commit
    SASS reset, HTML index framework, JavaScript audio playback

---
+++ JavaScript/audio.js
@@ -0,0 +1,18 @@
+	function playAudio(audio){
+	    var getAudio = document.getElementById(audio);
+	    getAudio.play();
+	}
+	
+	/*
+
+	---------Add the following to your HTML doc (somewhere above the JavaScript include) --------
+
+	<audio id="audio" src="audio.wma" preload="auto" controls></audio>
+
+	--------Add this as an event handler to your documents opening <body> tag----------
+
+	onload="playAudio('audio')"
+
+	---------In your HTML <head> element, add the following ------------
+	<script src="scripts/audio.js" >
+
