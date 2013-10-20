8765d80349142a4630a0728952f2136e6709631c
mjdugan14 <mjdugan14@gmail.com>
2013-10-20 14:36:15 -0400

    sky gradients

--- plain_snippets/server/installs.sh
+++ plain_snippets/server/installs.sh
@@ -1,20 +1,6 @@
 #! /bin/bash
 
-sudo apt-get install google-chrome-stable pencil gimp inkscape zsh terminator adobe-flashplugin opera evolution deluge krusader k3b vlc rhythmbox pianobar gwibber pidgin xchat redshift skype pidgin-skype synapse libreoffice dropbox gparted git firefox calibre
-echo "Installation finished"
-read -p "Move uppercase directories to lowercase?" yn
-case $yn in
-	[Yy]* ) mv ~/Documents/ ~/documents;echo "Documents=>documents";mv ~/Downloads/ ~/downloads;echo "Downloads=>downloads";mv ~/Music/ ~/music;echo "Music=>music";mv ~/Pictures/ ~/pictures;echo "Pictures=>pictures"; break;;
-	[Nn]* ) break;;
-read -p "Remove template and video directories?" yn
-case $yn in
-	[Yy]* ) rm -rf ~/Templates/;echo "Removed templates";rm -rf ~/Videos/;echo "Removed videos"; break;;
-	[Nn]* ) break;;
-read -p "Create web and source directories?" yn
-case $yn in
-	[Yy]* ) mkdir ~/src;echo "Source directory created"mkdir ~/web;echo "Web directory created"; break;;
-	[Nn]* ) break;;
-
+sudo apt-get install google-chrome-stable pencil gimp inkscape zsh terminator adobe-flashplugin opera evolution deluge krusader k3b vlc rhythmbox pianobar gwibber pidgin xchat redshift skype pidgin-skype synapse libreoffice dropbox gparted git firefox calibre vim openssh-server openvpn
 
 git clone https://github.com/mjdugan14/Code_Repo.git ~/src
 echo "Code repo cloned"
